de09645439fe7bc3f96d07a7d863f3557d1b8e2d
khushhallchandra <kcm00000@gmail.com>
2015-12-19 23:08:11 +0530

    Compeleted fisher faces implementation

--- fisherfaces.m
+++ fisherfaces.m
@@ -1,14 +1,25 @@
-function [W] = fisherfaces(X, IDs, k)
-
-[row col] = size(X);
-
-c = max(IDs);
-
-Vpca = pca(X, (col-c));
-
-X = X - kron(mu, ones([1 col]));
-Y = W'*X;
-
-Vlda = lda(Y, IDs, k);
-W = Wpca * Wlda ;
+function model = fisherfaces(X, IDs, k)
+  %%
+  %%    m_fisherface = fisherface(X, y, 100)
+  
+  N = size(X,2);
+  c = max(IDs);
+  
+  % set the num_components
+  
+  k = min(c-1, k);
+  
+  Pca = pca(X, (N-c));
+  X1=X-repmat(Pca.mu,1,size(X,2));
+  Y1=Pca.W'*X1;
+  Lda = lda(Y1, IDs, k);
+  
+  % build model
+  model.name = 'lda';
+  model.mu = repmat(0, size(X,1), 1);
+  model.D = Lda.D;
+  model.W = Pca.W*Lda.W;
+  model.P = model.W'*X;
+  model.k = Lda.k;
+  model.y = IDs;
 end
