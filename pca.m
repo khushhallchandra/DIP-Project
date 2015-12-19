e70f26275ce802927480bf0b5bdfd33161bd188a
khushhallchandra <kcm00000@gmail.com>
2015-12-19 22:41:47 +0530

    Updated lda and pca implementation

--- pca.m
+++ pca.m
@@ -1,26 +1,17 @@
-function [ V ] = pca(X, k)
-%	Performs a PCA on X and stores k principal components.
-%	X is columnized image matrix from dataset
-
-% 	r -> width*breadth of each image
-% 	c -> noOfTrainingFolder*noOfTrainingImages
-[r c]=size(X);
-
-% Mean Column Vector Calculation
-mu = mean(X,2);
-X = X - kron(mu, ones([1 c]));
-
-% Generate X'*X and find its eigenvectors
-[W,D] = eig(X'*X);
-
-% Get eigenvectors and eigenvalues of Covariance Matrix and normalize them
-V = X*W;
-norms = sqrt(sum(V.^2));
-V = V ./ kron(norms, ones([r 1]));
-
-% Sort eigenValues and vectors 
-[D, i] = sort(diag(D), 'descend');
-V = V(:,i);
-
-% keep k components
-V = V(:,1:k);
+function model = pca(X, k)
+  %%  Performs a PCA on X and stores num_components principal components.
+  %%
+  %%
+  % center data
+  mu = mean(X,2);
+  X = X - repmat(mu, 1, size(X,2));
+  % svd on centered data == pca
+  [E,D,V] = svd(X ,'econ');
+  % build model
+  model.name = 'pca';
+  model.D = diag(D).^2;
+  model.D = model.D(1:k);
+  model.W = E(:,1:k);
+  model.k = k;
+  model.mu = mu;
+end
